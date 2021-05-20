0f6372e074a96fbd42013a77d535d66feeee9bc1
Jstevens-Dev <54379237+Jstevens-Dev@users.noreply.github.com>
2021-05-20 15:06:47 -0400

    Update Small-Project_DB.sql

--- Small-Project_DB.sql
+++ Small-Project_DB.sql
@@ -19,3 +19,13 @@ LastName VARCHAR(50) NOT NULL DEFAULT '',
 UserName VARCHAR(50) NOT NULL DEFAULT '',
 Password VARCHAR(50) NOT NULL DEFAULT '', 
 PRIMARY KEY (UserID));
+
+INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "John", "Doe", "JDoe01", "Password");
+
+INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Jane", "Doe", "JaneDoe", "Password2");
+
+INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Albert", "Einstein", "AEinstien", "Password3");
+
+INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Carl", "Sagan", "CSagan", "Password4");
+
+INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Katie", "Bouman", "KBouman", "Password5");
