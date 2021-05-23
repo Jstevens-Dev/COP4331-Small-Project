b91722b8eccb984b537669a79017b98888e8b767
Jstevens-Dev <54379237+Jstevens-Dev@users.noreply.github.com>
2021-05-23 14:18:32 -0400

    Update Small-Project_DB.sql

--- Small-Project_DB.sql
+++ Small-Project_DB.sql
@@ -29,3 +29,25 @@ INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL
 INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Carl", "Sagan", "CSagan", "Password4");
 
 INSERT INTO Users (UserID, FirstName, LastName, UserName, Password) VALUES (NULL, "Katie", "Bouman", "KBouman", "Password5");
+
+CREATE TABLE Contacts (
+ContactID INT NOT NULL AUTO_INCREMENT,
+FirstName VARCHAR(50) NOT NULL DEFAULT '',
+LastName VARCHAR(50) NOT NULL DEFAULT '',
+Email VARCHAR(50) NOT NULL DEFAULT '',
+PhoneNO VARCHAR(50) NOT NULL DEFAULT '',
+DateCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+UserID INT,
+PRIMARY KEY (ContactID),
+FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
+
+INSERT INTO Contacts (ContactID, FirstName, LastName, Email, PhoneNO, UserID) VALUES (NULL, 'Srinivasa', 'Ramanujan', 'sRamanujan@fake.com', '000-000-0001', 1);
+
+INSERT INTO Contacts (ContactID, FirstName, LastName, Email, PhoneNO, UserID) VALUES (NULL, 'Abu', 'al-Khwarizmi', 'al-Khwarizmi@fake.com', '000-000-0002', 2);
+
+INSERT INTO Contacts (ContactID, FirstName, LastName, Email, PhoneNO, UserID) VALUES (NULL, 'Ellen', 'Ochoa', 'eOchoa@fake.com', '000-000-0003', 3);
+
+INSERT INTO Contacts (ContactID, FirstName, LastName, Email, PhoneNO, UserID) VALUES (NULL, 'Luis', 'Alvarez', 'lwAlvarez@fake.com', '000-000-0004', 4);
+
+INSERT INTO Contacts (ContactID, FirstName, LastName, Email, PhoneNO, UserID) VALUES (NULL, 'Chandrasekhara', 'Raman', 'cvRaman@fake.com', '000-000-0005', 5);
