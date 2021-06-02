211c33f41ccb5f93ba60a9c68f711b7804c89621
Jstevens-Dev <54379237+Jstevens-Dev@users.noreply.github.com>
2021-06-01 22:50:37 -0400

    Update SearchContact.php

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -12,15 +12,14 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("SELECT ContactID,FirstName,LastName,Email,PhoneNO FROM Contacts WHERE FirstName LIKE ? OR LastName LIKE ? OR Email LIKE ? OR PhoneNO LIKE ? AND UserID=?");
-		$firstName = "%" . $inData["firstname"] . "%";
-		$lastName = "%" . $inData["lastname"] . "%";
-		$email = "%" . $inData["email"] . "%";
-		$phoneNO = "%" . $inData["phoneNO"] . "%";
-		$userID = "%" . $inData["userID"] . "%";
+		$stmt = $conn->prepare("SELECT ContactID,FirstName,LastName,Email,PhoneNO FROM Contacts WHERE (((FirstName LIKE ? OR LastName LIKE ?) OR Email LIKE ?) OR PhoneNO LIKE ?) AND UserID = ?");
+		$firstName = "%" . $inData["search"] . "%";
+		$lastName = "%" . $inData["search"] . "%";
+		$email = "%" . $inData["search"] . "%";
+		$phoneNO = "%" . $inData["search"] . "%";
+		$userID = $inData["userID"];
 		$stmt->bind_param("sssss", $firstName, $lastName, $email, $phoneNO, $userID);
-		$stmt->execute();
-		
+		$stmt->execute();	
 		$result = $stmt->get_result();
 		
 		while($row = $result->fetch_assoc())
@@ -30,7 +29,7 @@
 				$searchResults .= ",";
 			}
 			$searchCount++;
-			$searchResults .= array('"' . $firstName . '"', '"' . $lastName . '"', '"' . $email . '"', '"' . $phoneNO . '"');
+			$searchResults .= '{"contactID" : "' . $row["ContactID"] . '", "firstname" : "' . $row["FirstName"] . '", "lastname" : "' . $row["LastName"] . '", "email" : "' . $row["Email"] . '", "phoneNO" : "' . $row["PhoneNO"] . '"}';
 		}
 		
 		if( $searchCount == 0 )
@@ -59,12 +58,13 @@
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"id":0,"firstname":"","lastname":"","email":"","phoneNO":"","error":"' . $err . '"}';
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
 	function returnWithInfo( $searchResults )
 	{
+		//$retValue = json_encode($searchResults);
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
